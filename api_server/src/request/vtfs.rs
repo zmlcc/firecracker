3ee4174f02f866483b7eba53012f7cd37e2f6350
Zhang Miaolei <zmlcc@outlook.com>
2019-11-19 10:18:48 +0800

    remove unused http_service.rs

--- api_server/src/request/vtfs.rs
+++ api_server/src/request/vtfs.rs
@@ -13,41 +13,15 @@ pub fn parse_put_vtfs(body: &Body, id_from_path: Option<&&str>) -> Result<Parsed
         }
     };
 
-    let vtfs = serde_json::from_slice::<VtfsDeviceConfig>(body.raw()).map_err(|e| {
+    let vtfs_cfg = serde_json::from_slice::<VtfsDeviceConfig>(body.raw()).map_err(|e| {
         Error::SerdeJson(e)
     })?;
-    if id != vtfs.drive_id.as_str() {
+    if id != vtfs_cfg.drive_id.as_str() {
         return Err(Error::Generic(
             StatusCode::BadRequest,
             "The id from the path does not match the id from the body!".to_string(),
         ));
     }
-    Ok(ParsedRequest::Sync(VmmAction::InsertVtfsDevice(vtfs)))
+    Ok(ParsedRequest::Sync(VmmAction::InsertVtfsDevice(vtfs_cfg)))
 }
 
-// impl IntoParsedRequest for VtfsDeviceConfig {
-//     fn into_parsed_request(
-//         self,
-//         id_from_path: Option<String>,
-//         _: Method,
-//     ) -> result::Result<ParsedRequest, String> {
-//         let id_from_path = id_from_path.unwrap_or_default();
-//         if id_from_path != self.drive_id.as_str() {
-//             return Err(String::from(
-//                 "The id from the path does not match the id from the body!",
-//             ));
-//         }
-
-//         let (sender, receiver) = oneshot::channel();
-//         Ok(ParsedRequest::Sync(
-//             VmmRequest::new(
-//             VmmAction::InsertVtfsDevice(self), sender),
-//             receiver,
-//         ))
-//     }
-// }
-
-#[cfg(test)]
-mod tests {
-
-}
